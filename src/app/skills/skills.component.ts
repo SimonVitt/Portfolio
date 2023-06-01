79e70f8722eea93e590e2a11c956b29d15aaabf7
Simon Vitt <ttivnomis@gmail.com>
2023-06-02 01:00:11 +0200

    updated projects

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,8 +7,7 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
-  skills = ['Django', 'Flask', 'Angular', 'TypeScript', 'Python', 'Google Cloud', 'Linux', 'Redis', 'Rest-Api', 'JavaScript', 'HTML', 'CSS', 'GIT', 'Scrum'];
-  skills_images = ['assets/img/icons/django-white.webp', 'assets/img/icons/flask-white.webp', 'assets/img/icons/angular.svg', 'assets/img/icons/typescript.svg', 'assets/img/icons/python-white.webp','assets/img/icons/google-cloud.webp', 'assets/img/icons/linux.webp', 'assets/img/icons/redis-white.webp', 'assets/img/icons/api.svg', 'assets/img/icons/javascript.svg', 'assets/img/icons/html.svg',
-  'assets/img/icons/css.svg', 'assets/img/icons/git.svg',
-  'assets/img/icons/scrum.svg'];
+  skills = ['Django', 'Flask', 'Angular', 'TypeScript', 'Python', 'Google Cloud', 'Linux', 'Redis', 'Rest-Api', 'SQL', 'JavaScript', 'HTML', 'CSS', 'GIT'];
+  skills_images = ['assets/img/icons/django-white.webp', 'assets/img/icons/flask-white.webp', 'assets/img/icons/angular.svg', 'assets/img/icons/typescript.svg', 'assets/img/icons/python-white.webp','assets/img/icons/google-cloud.webp', 'assets/img/icons/linux.webp', 'assets/img/icons/redis-white.webp', 'assets/img/icons/api.svg', 'assets/img/icons/sql.webp', 'assets/img/icons/javascript.svg', 'assets/img/icons/html.svg',
+  'assets/img/icons/css.svg', 'assets/img/icons/git.svg'];
 }
